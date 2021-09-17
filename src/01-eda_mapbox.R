b575b161d6528275e456cb9dcaf5d62af6c87d40
Garland Xie <garlandxie@gmail.com>
2021-09-17 16:39:23 -0400

    Initial commit

---
+++ src/01-eda_mapbox.R
@@ -0,0 +1,16 @@
+# libraries ----
+library(readr)
+library(here)
+library(skimr)
+
+# import ----
+mapbox <- read_csv(
+  here("data/raw", "0302231.wdwe-2h.csv")
+)
+
+# check packaging ----
+head(mapbox, n = 10)
+str(mapbox)
+
+# check names ----
+names(mapbox)
