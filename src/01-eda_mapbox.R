d26753b8c5d412b4ed03bd2eed3809434265ccba
Garland Xie <garlandxie@gmail.com>
2021-10-03 14:48:15 -0400

    Replaced with R 4.1's native pipes

--- src/01-eda_mapbox.R
+++ src/01-eda_mapbox.R
@@ -2,7 +2,7 @@
 library(readr)
 library(here)
 library(assertr)
-library(magrittr)
+library()
 
 # import ----
 mapbox <- read_csv(
@@ -19,14 +19,14 @@ names(mapbox)
 # validate ----
 
 # QA/QC b/c there's a lot of data (~ 1.3 million rows)
-mapbox %>% 
-  verify(xlat >= 43) %>%
-  verify(xlon <= 79) %>%
-  verify(agg_day_period >= 0) %>%
-  verify(agg_time_period >= 0) %>%
-  verify(activity_index_total >= 0) %>%
-  verify(month %in% c("2020-06", "2020-07", "2020-08")) %>%
-  assert(within_bounds(0, 1), agg_day_period) %>%
+mapbox |>
+  verify(xlat >= 43) |>
+  verify(xlon <= 79) |>
+  verify(agg_day_period >= 0) |>
+  verify(agg_time_period >= 0) |>
+  verify(activity_index_total >= 0) |>
+  verify(month %in% c("2020-06", "2020-07", "2020-08")) |>
+  assert(within_bounds(0, 1), agg_day_period) |>
   assert(within_bounds(0, 11), agg_time_period)
 
 
