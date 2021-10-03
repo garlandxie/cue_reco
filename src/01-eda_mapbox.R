54906b6e1b06e084d3a721359fe5a05c2f7a4610
Garland Xie <garlandxie@gmail.com>
2021-10-03 14:46:09 -0400

    Added more QC/QA checks

--- src/01-eda_mapbox.R
+++ src/01-eda_mapbox.R
@@ -18,9 +18,18 @@ names(mapbox)
 
 # validate ----
 
-errors <- mapbox %>% 
-  assertr::verify(xlat >= 43) %>%
-  assertr::verify(xlon <= 79) %>%
-  assertr::verify(agg_day_period >= 0) %>%
-  assertr::verify(agg_time_period >= 0)
+# QA/QC b/c there's a lot of data (~ 1.3 million rows)
+mapbox %>% 
+  verify(xlat >= 43) %>%
+  verify(xlon <= 79) %>%
+  verify(agg_day_period >= 0) %>%
+  verify(agg_time_period >= 0) %>%
+  verify(activity_index_total >= 0) %>%
+  verify(month %in% c("2020-06", "2020-07", "2020-08")) %>%
+  assert(within_bounds(0, 1), agg_day_period) %>%
+  assert(within_bounds(0, 11), agg_time_period)
+
+
+
+
   
