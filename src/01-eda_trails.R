2262273abb61ed01652a39eecfe7014c074d9f98
Garland Xie <garlandxie@gmail.com>
2021-10-23 14:06:25 -0400

    Added Conservation Halton Trails data

--- src/01-eda_trails.R
+++ src/01-eda_trails.R
@@ -10,11 +10,16 @@ trca_trails <- read_sf(
   here("data", "raw", "Trail_Strategy_trca.shp")
 )
 
+halt_trails <- read_sf(
+  here("data", "raw", "Halton_Trails.shp")
+)
+
 # check packaging ----
 skimr::skim(trca_trails)
+skimr::skim(halt_trails)
 
 # plots -----
 
-trca_trails |>
-  ggplot() +
-  geom_sf()
+ggplot() + 
+  geom_sf(data = halt_trails) + 
+  geom_sf(data = trca_trails)
