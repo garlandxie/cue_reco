8680fe2da30c1e841538c7a45bf2b762fe041671
Garland Xie <garlandxie@gmail.com>
2022-01-09 14:26:25 -0500

    Import RDS file of mapbox (data table class)

--- src/01-eda_mapbox.R
+++ src/01-eda_mapbox.R
@@ -1,40 +1,13 @@
 # libraries -------------------------------------------------------------------- 
 library(readr)       # for uploading csv very quickly
 library(here)        # for creating relative file paths 
-library(assertr)     # for validating data
 library(data.table)  # for manipulating data faster than dplyr
 library(magrittr)    # for 'piping' data table code 
 library(ggplot2)     # for visualizing data 
 library(patchwork)
 
 # import ----
-mapbox <- read_csv(
-  here("data/raw", "0302231.wdwe-2h.csv")
-)
-
-# check packaging ----
-head(mapbox, n = 10)
-str(mapbox)
-
-# check names ----
-names(mapbox)
-
-# validate ----
-
-# QA/QC b/c there's a lot of data (~ 13 million rows)
-mapbox %>%
-  verify(xlat >= 43) %>%
-  verify(xlon <= 79) %>%
-  verify(agg_day_period >= 0) %>%
-  verify(agg_time_period >= 0) %>% 
-  verify(activity_index_total >= 0) %>% 
-  verify(month %in% c("2020-06", "2020-07", "2020-08")) %>% 
-  assert(within_bounds(0, 1), agg_day_period) %>% 
-  assert(within_bounds(0, 11), agg_time_period)
-
-# convert to data table --------------------------------------------------------
-
-mapbox_dt <- as.data.table(mapbox)
+mapbox_dt <- readRDS(here("data", "working", "mapbox_dt.RDS"))
 
 # data cleaning: number of visitors per weekday --------------------------------
 
